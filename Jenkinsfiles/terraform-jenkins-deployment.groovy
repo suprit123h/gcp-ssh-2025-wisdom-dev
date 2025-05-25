1eb82c9cafeb4701310fdb11d30e48e777a003db
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-25 20:17:14 +0530

    Update terraform-jenkins-deployment.groovy

--- Jenkinsfiles/terraform-jenkins-deployment.groovy
+++ Jenkinsfiles/terraform-jenkins-deployment.groovy
@@ -20,11 +20,11 @@ pipeline {
 				),
 			    usernamePassword(
 				    credentialsId: "81dead19-04f7-4a8a-91c2-84adc1e8b1f1",
-				    usernameVariable: "suprit123h",
-				    passwordVariable: "suprit123h"
+				    usernameVariable: "git_username",
+				    passwordVariable: "git_password"
 				    )
 			    ]){
-			    sh "echo $usernameVariable"
+			    sh "echo $git_username, $git_password"
                     println('Hello This is the git passkey token name: "Surpass"')
 			sh "pwd ${WORKSPACE}"
 
