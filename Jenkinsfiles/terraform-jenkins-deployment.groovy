827b9ab78b9a5173e475e07f5d38a96ea71542ed
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-25 19:40:01 +0530

    Update terraform-jenkins-deployment.groovy

--- Jenkinsfiles/terraform-jenkins-deployment.groovy
+++ Jenkinsfiles/terraform-jenkins-deployment.groovy
@@ -12,6 +12,14 @@ pipeline {
             steps {
 	    	dir("infraGCP"){
                 script {
+
+		    withCredentials([
+			file(
+				credentialsId: "ssh-devops1-dev-2025",
+				variables: 'GOOGLE_APPLICATION_CREDENTIALS'
+				)
+			    ])
+		sh "cat GOOGLE_APPLICATION_CREDENTIALS"
                     println('Hello This is the git passkey token name: "Surpass"')
 			sh "pwd ${WORKSPACE}"
 
