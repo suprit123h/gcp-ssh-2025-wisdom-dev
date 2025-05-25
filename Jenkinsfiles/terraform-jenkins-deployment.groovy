fca22ed5f539f9f289819dc7abf0d74d07b3fef9
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-25 18:49:36 +0530

    Update terraform-jenkins-deployment.groovy

--- Jenkinsfiles/terraform-jenkins-deployment.groovy
+++ Jenkinsfiles/terraform-jenkins-deployment.groovy
@@ -18,9 +18,9 @@ pipeline {
 
 		    sh "git pull https://github.com/suprit123h/gcp-ssh-2025-wisdom-dev.git"
 
-		    sh "/var/jenkins_home/workspace/DevOps-Workspace/terraformjobs/bucket_deployment_gcp/infraGCP/gcp-ssh-2025-wisdom-dev/terraform init"
+		    sh "gcp-ssh-2025-wisdom-dev/terraform init"
 
-		    sh "/var/jenkins_home/workspace/DevOps-Workspace/terraformjobs/bucket_deployment_gcp/infraGCP/gcp-ssh-2025-wisdom-dev/terraform plan"
+		    sh "gcp-ssh-2025-wisdom-dev/terraform plan"
 
 		    //sh "gcloud version"
 		   }
