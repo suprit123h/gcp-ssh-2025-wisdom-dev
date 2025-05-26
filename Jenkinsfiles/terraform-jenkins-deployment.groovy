874c7ef9a841a09170e78cbb7d0d8768283bd02e
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-26 22:48:37 +0530

    Update terraform-jenkins-deployment.groovy

--- Jenkinsfiles/terraform-jenkins-deployment.groovy
+++ Jenkinsfiles/terraform-jenkins-deployment.groovy
@@ -25,7 +25,7 @@ pipeline {
 					// Optionally, ensure the repo is checked out before pulling
 					checkout([$class: 'GitSCM', branches: [[name: '*/master']], userRemoteConfigs: [[url: 'https://github.com/suprit123h/gcp-ssh-2025-wisdom-dev.git']]])
 					// git pull "https://github.com/suprit123h/gcp-ssh-2025-wisdom-dev.git"
-					sh "ls -lrth"
+					sh "ls"
 
 					sh '''
 						git pull
