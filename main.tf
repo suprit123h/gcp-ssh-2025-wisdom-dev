6bd695d6c115cae240f98dee13f5472d9b0456f1
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-27 10:32:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_project_service" "cloud_resource_manager" {
-  project = var.project_id
+  project = var.project
   service = "cloudresourcemanager.googleapis.com"
   disable_dependent_services = true
 }
