b1f94fd55f095c32c24cbeabebd60fb75bb2e8b9
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-05-28 18:23:50 +0530

    Create ssh-devops1-dev-2025.tf

---
+++ ssh-devops1-dev-2025.tf
@@ -0,0 +1,9 @@
+module "gcs_bucket" {
+  source              = "git@github.com:suprit123h/terraform-modules-bucket.git?ref=main"
+  ./terraform-modules/gcs-bucket"
+  bucket_name         = "my-gcs-bucket-ssh-2025"
+  location            = "us-central1"
+  storage_class       = "STANDARD"
+  versioning_enabled  = true
+  object_lifecycle_days = 60
+}
