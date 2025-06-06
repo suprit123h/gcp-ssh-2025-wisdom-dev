7dee2fa418765d38f5ddb4f8a03a7799b4e583c2
suprit123h <56013767+suprit123h@users.noreply.github.com>
2025-06-06 19:48:58 +0530

    Delete main_backup.tf

--- main_backup.tf
+++
@@ -1,6 +0,0 @@
-module "gcs_bucket" {
-  source        = "git@github.com:suprit123h/terraform-modules-bucket.git"
-  bucket_name   = var.config["bucket_name"]
-  location      = var.config["location"]
-  storage_class = var.config["storage_class"]
-}
